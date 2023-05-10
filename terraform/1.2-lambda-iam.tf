ef4782a553293bd5764a70e7ade685830ce82f2e
MOHAMMAD SUMAIR <91409244+sumair-aslam09@users.noreply.github.com>
2023-05-10 11:17:22 +0530

    Update 1.2-lambda-iam.tf

--- terraform/1.2-lambda-iam.tf
+++ terraform/1.2-lambda-iam.tf
@@ -5,7 +5,6 @@ resource "aws_iam_role" "hello_lambda_exec" {
 }
 
 resource "aws_iam_role_policy_attachment" "hello_lambda_policy" {
-    name = "hello_lambda_policy"
     role = aws_iam_role.hello_lambda_exec.name
     policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
